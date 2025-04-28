93458a0fccdd06d0137e53c538f4e92c1eb235b6
djain18 <dakshinjain187@gmail.com>
2025-04-28 15:35:24 +0530

    feat: add logic of rock paper scissors

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice(){
+//     Step 1: Create a new variable named randomNumber.
+    let randomNumber;
+// Step 2: Store the random number generated to randomNumber variable.
+randomNumber = Math.random();
+// Step 3: Compare the values of the randomNumber variable.
+if(randomNumber < 0.5){
+    return "Rock";
+} else if(randomNumber > 0.5){
+    return "Scissors";
+} else {
+    return "Paper";
+}
+// Step 4: If variable is less than 0.5 print Rock if more than 0.5 print scissors if exact middle print paper.
+// Step 5: return the output
+}
+
+console.log(getComputerChoice());
